a6fe8a96b6d8103eddbea9af161a4965e4b9bec3
Francis Engelmann <engelmann@vision.rwth-aachen.de>
2014-12-01 16:57:06 +0100

    added first function

--- apps/demo.cpp
+++ apps/demo.cpp
@@ -1,16 +1,23 @@
 #include <stdlib.h>
 #include <iostream>
 #include <opencv2/highgui/highgui.hpp>
+#include <sprit/sprit.h>
 
 int main(int agrc, char * argv[])
 {
     std::cout << "Hello, World!" << std::endl;
 
-    cv::Mat img = cv::imread("/home/engelmann/Programming/sprit/data/training/000000_0.JPG");
-    cv::imshow("First image",img);
+    // Load the images
+    cv::Mat img1 = cv::imread("/home/engelmann/Programming/sprit/data/training/000000_0.JPG");
     cv::Mat img2 = cv::imread("/home/engelmann/Programming/sprit/data/training/000000_1.JPG");
-    cv::imshow("Second image",img2);
-    cv::waitKey(0);
+    //cv::imshow("img1",img1);
+    //cv::imshow("img2",img2);
 
+    // Process the images
+    sprit::processImage(img1);
+    cv::imshow("img1p",img1);
+    sprit::processImage(img2);
+    cv::imshow("img2p",img2);
+    cv::waitKey(0);
     return 0;
 }
